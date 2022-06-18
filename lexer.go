963b5f1c8bb27cf1c8a424d736eb3497a8a99607
ainzsoup <gamrauis@gmail.com>
2022-06-18 01:11:57 +0100

    initial commit

---
+++ lexer.go
@@ -0,0 +1,26 @@
+package lexer
+
+type lexer struct {
+	input string
+	position int // current position in input (points to current char)
+	readPosition int // current reading position in input (after current char)
+	cha  byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
